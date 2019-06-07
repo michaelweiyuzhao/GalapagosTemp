e872a57a7cf58d672993505b5dcd6686b093f1e5
Zhao  Weiyu <zhaowe32@ug244.eecg.toronto.edu>
2019-06-07 10:51:59 -0400

    started modifying udp_bridge.cpp to read/write every cycle

--- udp_bridge.cpp
+++ udp_bridge.cpp
@@ -17,7 +17,9 @@ ap_uint <64> reverse_endian_512_tkeep(ap_uint <64> X) {
 
 void udp_to_app(
     hls::stream <udp_axis> & from_udp,
-    hls::stream <app_axis> & to_app
+    hls::stream <app_axis> & to_app,
+    hls::stream <meta> & meta_from_udp,
+    hls::stream <meta> & meta_to_app
 ) {
 
 #pragma HLS PIPELINE II=1
@@ -26,76 +28,65 @@ void udp_to_app(
 #define STREAM_UDP_TO_APP 1
 #define END_UDP_TO_APP 2
 
-    static udp_axis udp_axis_packet;
+    static udp_axis buf[2];
+    static ap_uint <1> bufen[2];
+    static ap_uint <1> last = 1;
     static app_axis app_axis_packet;
-    static ap_uint <496> tdata_buf; //512 - 16 = 496, tdest takes 16 bits
-    static ap_uint <62> tkeep_buf; //64 - 2 = 62, tdest takes 2 bytes
-    static int state = INIT_UDP_TO_APP;
 
-    switch(state){
-        case INIT_UDP_TO_APP:{
-            if(!from_udp.empty()){
-                udp_axis_packet = from_udp.read();
-                app_axis_packet.tdest = udp_axis_packet.tdata.range(511,496);
-                if(udp_axis_packet.tlast){ // all data is present, send
-                    app_axis_packet.tdata.range(511,16) = udp_axis_packet.tdata.range(495,0);
-                    app_axis_packet.tdata.range(15,0) = ap_uint <16> (0);
-                    app_axis_packet.tkeep.range(63,2) = udp_axis_packet.tkeep.range(61,0);
-                    app_axis_packet.tkeep.range(1,0) = ap_uint <2> (0);
-                    app_axis_packet.tlast = udp_axis_packet.tlast; //1
-                    to_app.write(app_axis_packet);
-                    state = INIT_UDP_TO_APP;
-                    break;
-                }
-                else{ // buffer and wait for bottom 2 bytes in next flit
-                    tdata_buf = udp_axis_packet.tdata.range(495,0);
-                    tkeep_buf = udp_axis_packet.tkeep.range(61,0);
-                    state = STREAM_UDP_TO_APP;
-                    break;
-                }
-            }
-            state = INIT_UDP_TO_APP;
-            break;
+    // buf[1] logic
+    if(!bufen[1]){ // if buf1 is empty shift in buf0
+        buf[1] = _buf[0];
+        bufen[1] = bufen[0];
+    }
+
+    // buf[0] logic
+    if(!from_udp.empty()){ // if packet is valid read packet into buf0
+        buf[0] = from_udp.read();
+        bufen[0] = 1;
+    }
+
+    // if bufen[1] is 0, do nothing
+    // if bufen[1] is 1, check last
+    // if last, buf[1] has tdest
+    // if not last, buf[1] does not have tdest
+    // if buf[1] is last, send buf[1], bufen[1] is 0
+    // if buf[1] is not last, check buf[0]
+    // if bufen[0] is 0, do nothing
+    // if bufen[0] is 1, check buf[0] last
+    // if buf[0] is last, send buf[1] + buf[0], bufen[1] is 0, check buf[0] tkeep
+    // if buf[0] does not have data left, bufen[0] is 0
+    // if buf[0] has data left, bufen[0] is 1
+    // if buf[0] is not last, send buf[1] + buf[0]
+
+    if(bufen[1]){
+        app_axis_packet.tdest = last ? buf[1].tdata.range(511,496) : app_axis_packet.tdest;
+        if(buf[1].tlast){
+            app_axis_packet.tdata.range(511,16) = buf[1].tdata.range(495,0);
+            app_axis_packet.tdata.range(15,0) = ap_uint <16> (0);
+            app_axis_packet.tkeep.range(63,2) = buf[1].tkeep.range(61,0);
+            app_axis_packet.tkeep.range(1,0) = ap_uint <2> (0);
+            app_axis_packet.tlast = buf[1].tlast; //1
+            bufen[1] = 0;
+            to_app.write(app_axis_packet);
         }
-        case STREAM_UDP_TO_APP:{
-            if(!from_udp.empty()){
-                udp_axis_packet = from_udp.read();
-                app_axis_packet.tdata.range(511,16) = tdata_buf;
-                app_axis_packet.tdata.range(15,0) = udp_axis_packet.tdata.range(511,496);
-				app_axis_packet.tkeep.range(63,2) = tkeep_buf;
-				app_axis_packet.tkeep.range(1,0) = udp_axis_packet.tkeep.range(63,62);
-                tdata_buf = udp_axis_packet.tdata.range(495,0);
-                tkeep_buf = udp_axis_packet.tkeep.range(61,0);
-                if(udp_axis_packet.tlast){
-                    if(udp_axis_packet.tkeep[61]){ //extra data exists that doesn't fit
-                        app_axis_packet.tlast = 0;
-                        state = END_UDP_TO_APP;
-                    }
-                    else{ // all data fits in last packet
-                        app_axis_packet.tlast = udp_axis_packet.tlast; //1
-                        state = INIT_UDP_TO_APP;
-                    }
+        else{
+            if(bufen[0]){
+                app_axis_packet.tdata.range(511,16) = buf[1].tdata.range(495,0);
+                app_axis_packet.tdata.range(15,0) = buf[0].tdata.range(511,496);
+                app_axis_packet.tkeep.range(63,2) = buf[1].tkeep.range(61,0);
+                app_axis_packet.tkeep.range(1,0) = buf[0].tkeep.range(63,62);
+                if(buf[0].tlast){
+                    bufen[0] = buf[0].tkeep[61]; // if more data exists, bufen0 = 1 else bufen0 = 0
+                    app_axis_packet.tlast = !buf[0].tkeep[61]; // if more data exists, tlast = 0 else tlast = 1
                 }
                 else{
-                    app_axis_packet.tlast = udp_axis_packet.tlast; //0
-                    state = STREAM_UDP_TO_APP;
+                    app_axis_packet.tlast = buf[0].tlast; //0
                 }
+                bufen[1] = 0;
                 to_app.write(app_axis_packet);
-                break;
             }
-            state = STREAM_UDP_TO_APP;
-            break;
         }
-        case END_UDP_TO_APP:{
-            app_axis_packet.tdata.range(511,16) = tdata_buf;
-            app_axis_packet.tdata.range(15,0) = ap_uint <16> (0);
-            app_axis_packet.tkeep.range(63,2) = tkeep_buf;
-            app_axis_packet.tkeep.range(1,0) = ap_uint <2> (0);
-            app_axis_packet.tlast = udp_axis_packet.tlast; //1
-            to_app.write(app_axis_packet);
-            state = INIT_UDP_TO_APP;
-            break;
-        }
+        last = app_axis_packet.tlast;
     }
 }
 
@@ -189,14 +180,16 @@ void app_to_udp(
 }
 
 void udp_bridge (
+    hls::stream <udp_axis> & from_udp,
     hls::stream <app_axis> & to_app,
     hls::stream <app_axis> & from_app,
-    hls::stream <udp_axis> & to_udp,
-    hls::stream <udp_axis> & from_udp
+    hls::stream <udp_axis> & to_udp
     // ,
 
-    // hls::stream <meta> & meta_in,
-    // hls::stream <meta> & meta_out
+    // hls::stream <meta> & meta_from_udp
+    // hls::stream <meta> & meta_to_app,
+    // hls::stream <meta> & meta_from_app,
+    // hls::stream <meta> & meta_to_udp,
 ) {
 
 #pragma HLS DATAFLOW
